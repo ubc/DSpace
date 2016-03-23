4db7ed7bf72d3559f92e8a55aa67d7126d1fc7fb
Davor Cubranic <cubranic@stat.ubc.ca>
2016-03-23 16:43:14 -0700

    Create UI for submitting item evaluations

---
+++ dspace-jspui/src/main/java/org/dspace/app/webui/servlet/EvaluateItemServlet.java
@@ -0,0 +1,106 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.webui.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.dspace.app.webui.util.JSPManager;
+import org.dspace.app.webui.util.UIUtil;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Evaluation;
+import org.dspace.content.Item;
+import org.dspace.core.Context;
+import org.dspace.core.LogManager;
+import org.dspace.eperson.EPerson;
+
+/**
+ * Servlet for handling editing user profiles
+ * 
+ * @author Robert Tansley
+ * @version $Revision$
+ */
+public class EvaluateItemServlet extends DSpaceServlet
+{
+    /** Logger */
+    private static final Logger log = Logger.getLogger(EvaluateItemServlet.class);
+
+    @Override
+    protected void doDSGet(Context context, HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException,
+            SQLException, AuthorizeException
+    {
+        doDSPost(context, request, response);
+    }
+
+    @Override
+    protected void doDSPost(Context context, HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException,
+            SQLException, AuthorizeException
+    {
+        log.info(LogManager.getHeader(context, "evaluate_item", 
+                request.getParameter("item_id")));
+
+        // get the values out of the request
+        int itemId = UIUtil.getIntParameter(request, "item_id");
+        Item item = Item.find(context, itemId);
+
+    	String buttonPressed = UIUtil.getSubmitButton(request, "submit_evaluate");
+    	
+    	if (buttonPressed.equals("submit")) {
+            processEvaluation(context, request, response, item);
+        }
+        else {
+            showEvaluationForm(request, response, item);
+        }
+     }
+    
+    
+    private void showEvaluationForm(HttpServletRequest request,
+            HttpServletResponse response, Item item) 
+        throws ServletException, IOException, SQLException, AuthorizeException
+    {
+        request.setAttribute("item", item);
+        JSPManager.showJSP(request, response, "/mydspace/evaluate-item.jsp");
+    }
+    
+    
+    private void processEvaluation(Context context, HttpServletRequest request,
+            HttpServletResponse response, Item item)
+        throws ServletException, IOException, SQLException, AuthorizeException
+    {
+        // Get the user - authentication should have happened
+        EPerson eperson = context.getCurrentUser();
+
+        String evaluation = request.getParameter("item-evaluation");
+        
+        if (request.getParameter("submit") != null && !StringUtils.isEmpty(evaluation)) {
+            // record submission
+            Evaluation.create(context, eperson, item, evaluation);
+            
+            log.info(LogManager.getHeader(context, "evaluate_item", "completed"));
+            
+            // Show confirmation
+            JSPManager.showJSP(request, response, "/mydspace/task-complete.jsp");
+
+            context.complete();
+        }
+        else {
+            log.info(LogManager.getHeader(context, "evaluate_item", "incomplete"));
+
+            request.setAttribute("incomplete", Boolean.TRUE);
+            showEvaluationForm(request, response, item);
+        }
+    }
+}
---
+++ dspace-jspui/src/main/webapp/mydspace/evaluate-item.jsp
@@ -0,0 +1,63 @@
+<%--
+
+    The contents of this file are subject to the license and copyright
+    detailed in the LICENSE and NOTICE files at the root of the source
+    tree and available online at
+
+    http://www.dspace.org/license/
+
+--%>
+<%@page import="org.dspace.app.webui.util.UIUtil"%>
+<%--
+  - Item evaluation form
+  -
+  - Attributes to pass in:
+  -
+  -   eperson     - the EPerson who's editing their profile
+  -   item        - the Item being evaluated
+  --%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
+<%@ taglib uri="http://www.dspace.org/dspace-tags.tld" prefix="dspace" %>
+
+<%@ page import="javax.servlet.jsp.jstl.fmt.LocaleSupport" %>
+
+<%@ page import="org.dspace.eperson.EPerson" %>
+<%@ page import="org.dspace.content.Item" %>
+
+<%
+    Item item = (Item) request.getAttribute("item");
+    
+    Boolean attr = (Boolean) request.getAttribute("incomplete");
+    boolean incomplete = (attr != null && attr.booleanValue());
+%>
+
+<dspace:layout style="submission" title="Evaluate Item" nocache="true">
+
+    <h1>Pedagogical Evaluation for item <%= item.getName() %></h1>
+
+<%
+    if (incomplete) {
+%>
+    <p class="alert alert-info"><strong>Please fill out the evaluation.</strong></p>
+<%
+    }
+%>
+
+    <form class="" action="<%= request.getContextPath() %>/evaluate" method="post">
+        <input type="hidden" name="item_id" value="<%= item.getID() %>"/>
+
+        <div class="form-group">
+            <label for="item-evaluation">Evaluation</label>
+            <textarea name="item-evaluation" rows=6 id="item-evaluation" class="form-control"></textarea>
+            <p class="help-block">Please enter your pedagogical evaluation of the item.</p>
+        </div>
+
+ 	<div class="col-md-offset-5">
+	   <input class="btn btn-success col-md-4" type="submit" name="submit" value="Submit evaluation" />
+	 </div>
+   </form>
+</dspace:layout>
