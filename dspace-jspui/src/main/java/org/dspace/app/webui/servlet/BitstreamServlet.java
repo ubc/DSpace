e88dbbde602e5d64da08881c3af78f9f1eddbdd1
John Hsu <john.hsu@ubc.ca>
2018-06-15 13:06:24 -0700

    FIX can't skip around videos in Chrome.
    Chrome requires the server to implement partial content requests in
    order to support seek operations. I've kludged together partial content
    request support for the BitstreamServlet. This should also let videos be
    a bit more responsive since browsers don't have to download the entire
    video anymore.
    
    Implements #46.

--- dspace-jspui/src/main/java/org/dspace/app/webui/servlet/BitstreamServlet.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/servlet/BitstreamServlet.java
@@ -9,7 +9,10 @@ package org.dspace.app.webui.servlet;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -228,10 +231,6 @@ public class BitstreamServlet extends DSpaceServlet
 		// Set the response MIME type
         response.setContentType(bitstream.getFormat().getMIMEType());
 
-        // Response length
-        response.setHeader("Content-Length", String
-                .valueOf(bitstream.getSize()));
-
 		if(threshold != -1 && bitstream.getSize() >= threshold)
 		{
 			UIUtil.setBitstreamDisposition(bitstream.getName(), request, response);
@@ -240,8 +239,167 @@ public class BitstreamServlet extends DSpaceServlet
         //DO NOT REMOVE IT - WE NEED TO FREE DB CONNECTION TO AVOID CONNECTION POOL EXHAUSTION FOR BIG FILES AND SLOW DOWNLOADS
         context.complete();
 
-        Utils.bufferedCopy(is, response.getOutputStream());
+        // Prepare some variables. The full Range represents the complete file.
+        long length = bitstream.getSize();
+        Range full = new Range(0, length - 1, length);
+        List<Range> ranges = new ArrayList<Range>();
+
+        // Validate and process Range and If-Range headers.
+        String range = request.getHeader("Range");
+        if (range != null) {
+
+            // Range header should match format "bytes=n-n,n-n,n-n...". If not, then return 416.
+            if (!range.matches("^bytes=\\d*-\\d*(,\\d*-\\d*)*$")) {
+                response.setHeader("Content-Range", "bytes */" + length); // Required in 416.
+                response.sendError(HttpServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE);
+                return;
+            }
+
+            // If any valid If-Range header, then process each part of byte range.
+            if (ranges.isEmpty()) {
+                for (String part : range.substring(6).split(",")) {
+                    // Assuming a file with length of 100, the following examples returns bytes at:
+                    // 50-80 (50 to 80), 40- (40 to length=100), -20 (length-20=80 to length=100).
+                    long start = sublong(part, 0, part.indexOf("-"));
+                    long end = sublong(part, part.indexOf("-") + 1, part.length());
+
+                    if (start == -1) {
+                        start = length - end;
+                        end = length - 1;
+                    } else if (end == -1 || end > length - 1) {
+                        end = length - 1;
+                    }
+
+                    // Check if Range is syntactically valid. If not, then return 416.
+                    if (start > end) {
+                        response.setHeader("Content-Range", "bytes */" + length); // Required in 416.
+                        response.sendError(HttpServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE);
+                        return;
+                    }
+
+                    // Add range.
+                    ranges.add(new Range(start, end, length));
+                }
+            }
+        }
+        response.setHeader("Accept-Ranges", "bytes");
+
+        // Send requested file (part(s)) to client ------------------------------------------------
+
+        // Prepare streams.
+        OutputStream output = null;
+
+		// Open streams.
+		output = response.getOutputStream();
+
+		if (ranges.size() == 1 && !ranges.get(0).equals(full)) {
+			log.debug("Dealing with a partial content request.");
+			// Deal with a partial content request
+			Range r = ranges.get(0);
+			response.setHeader("Content-Length", String.valueOf(r.length));
+			response.setHeader("Content-Range", "bytes " + r.start + "-" + r.end + "/" + r.total);
+			response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT); // 206.
+			// Copy single part range.
+			int bytesRead = partialContent(is, output, r.start, r.length);
+
+		}
+		else {
+			// Response length
+			response.setHeader("Content-Length", String
+					.valueOf(bitstream.getSize()));
+			Utils.bufferedCopy(is, response.getOutputStream());
+
+		}
         is.close();
         response.getOutputStream().flush();
+    }
+
+	/**
+	 * For dealing with partial content requests, sends only parts of input to the output.
+	 * @param input the input stream to read from
+	 * @param output the output stream to write the contents from input to
+	 * @param start the byte to start reading input at
+	 * @param length the amount of bytes to read
+	 * @return
+	 * @throws IOException 
+	 */
+    private static int partialContent(InputStream input, OutputStream output, long start, long length)
+		throws IOException
+    {
+		int bufferSize = 1024*1024; // 1 MiB of buffer
+		long remaining = length;
+		int totalBytesRead = 0;
+		// can't put everything into the buffer all at once cause we'll run out
+		// of heap space on large files, so going to read the input in small chunks
+		byte[] partialContentBuffer = new byte[bufferSize];
+
+		input.skip((int)start);
+
+		while (remaining > 0)
+		{
+			int bytesRead = input.read(partialContentBuffer, 0, bufferSize);
+			if (bytesRead < 0)  {
+				log.error("Partial Contest Request read error");
+				break;
+			}
+			output.write(partialContentBuffer, 0, bytesRead);
+			totalBytesRead += bytesRead;
+			remaining -= bytesRead;
+		}
+		return totalBytesRead;
+    }
+
+    /**
+     * Returns a substring of the given string value from the given begin index to the given end
+     * index as a long. If the substring is empty, then -1 will be returned
+     * @param value The string value to return a substring as long for.
+     * @param beginIndex The begin index of the substring to be returned as long.
+     * @param endIndex The end index of the substring to be returned as long.
+     * @return A substring of the given string value as long or -1 if substring is empty.
+     */
+    private static long sublong(String value, int beginIndex, int endIndex) {
+        String substring = value.substring(beginIndex, endIndex);
+        return (substring.length() > 0) ? Long.parseLong(substring) : -1;
+    }
+
+    /**
+     * This class represents a byte range.
+     */
+    protected class Range {
+        public long start;
+        public long end;
+        public long length;
+        public long total;
+
+        /**
+         * Construct a byte range.
+         * @param start Start of the byte range.
+         * @param end End of the byte range.
+         * @param total Total length of the byte source.
+         */
+        public Range(long start, long end, long total) {
+            this.start = start;
+            this.end = end;
+            this.length = end - start + 1;
+            this.total = total;
+        }
+
+		@Override public boolean equals(Object obj) {
+			if (obj == null) return false;
+			if (!Range.class.isAssignableFrom(obj.getClass())) {
+				return false;
+			}
+			final Range other = (Range) obj;
+			
+			if (other.start == start &&
+				other.end == end &&
+				other.length == length &&
+				other.total == total) {
+				return true;
+			}
+
+			return false;
+		}
+
     }
 }
