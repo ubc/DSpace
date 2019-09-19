4d4e857a618e544a446be710fc38b8594456ed95
John Hsu <john.hsu@ubc.ca>
2019-09-18 17:14:51 -0700

    FIX missing import killing build

--- dspace-jspui/src/main/java/org/dspace/app/webui/ubc/UBCDiscoverySearchRequestProcessor.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/ubc/UBCDiscoverySearchRequestProcessor.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Map;
 
